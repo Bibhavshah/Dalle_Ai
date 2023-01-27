917c2469d5f733d299a236dac410d6fae4365c79
Bibhavshah <78656387+Bibhavshah@users.noreply.github.com>
2023-01-27 11:52:31 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dall-e-clone-lu5j.onrender.com/api/v1/post', {
+      const response = await fetch('https://dalle-ai-jby5.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
